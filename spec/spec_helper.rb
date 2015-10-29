f6c0efc8d81520c283b0ebe47752dde110676e8f
Zach Toolson <zach.toolson@mx.com>
2015-10-29 16:27:36 -0600

    create parity with ignorable gem under the ignore_columns name

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,7 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'ignore_columns'
+require 'active_record'
+
+puts "Testing against ActiveRecord #{ActiveRecord::VERSION::STRING}"
+
+ActiveRecord::Base.establish_connection("adapter" => "sqlite3", "database" => ":memory:")
